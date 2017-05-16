be6f0aa22f4ca1621700b564d4fe60d4d43b1b04
JamesHawking <jakubj91@gmail.com>
2017-05-16 06:05:07 +0200

    Camera!!!

--- src/pages/addSlabForm/addSlabForm.ts
+++ src/pages/addSlabForm/addSlabForm.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { AngularFire, FirebaseListObservable, AngularFireDatabase } from 'angularfire2';
 
+
 import { AuthService } from '../../providers/auth-service';
 
 import { Geolocation } from '@ionic-native/geolocation';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'page-addSlabForm',
@@ -12,20 +15,60 @@ import { Geolocation } from '@ionic-native/geolocation';
 })
 export class AddSlabForm {
   items: FirebaseListObservable<any[]>;
-  slab = {};
+  slab = {
+    img_url: ''
+  };
   coords = {};
+  public base64Image: string;
+  storagePicsRef: any;
+  objectToSave: Array<any> = new Array;
+  picUrl: any;
 
-  constructor(public navCtrl: NavController,af: AngularFire, db: AngularFireDatabase,private _auth: AuthService, private geolocation: Geolocation) {
+  constructor(private camera: Camera, public navCtrl: NavController, af: AngularFire, db: AngularFireDatabase, private _auth: AuthService, private geolocation: Geolocation) {
     this.items = af.database.list('/slabs');
 
     this.geolocation.getCurrentPosition().then((position) => {
-       this.coords = { lat: position.coords.latitude, lng: position.coords.longitude }
-        console.log(position.coords.latitude, position.coords.longitude)}, (err) => {
-        console.log(err);
+      this.coords = { lat: position.coords.latitude, lng: position.coords.longitude }
+      console.log(position.coords.latitude, position.coords.longitude)
+    }, (err) => {
+      console.log(err);
     });
+
+       this.storagePicsRef = firebase.storage().ref().child('pictures/');
+  
 
   }
 
+  getPic() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64:
+      this.base64Image = 'data:image/jpeg;base64,' + imageData; //
+      this.uploadPicture(imageData);
+    }, (err) => {
+      // Handle error
+    });
+  }
+
+  uploadPicture(imgData: any) {
+    var randomNumber = Math.floor(Math.random() * 256);
+    console.log('Random number : ' + randomNumber);
+
+    this.storagePicsRef.child(randomNumber + '.jpg').putString(imgData, 'base64', { contentType: 'image/jpeg' }).then((savedPicture) => {
+      console.log('saved picture URL', savedPicture.downloadURL);
+      this.picUrl = savedPicture.downloadURL;
+      this.objectToSave.push(savedPicture.downloadURL);
+      console.log('objectToSave : ' + JSON.stringify(this.objectToSave));
+      this.storagePicsRef.set(this.objectToSave);
+    });
+  }
   // signInWithFacebook(): void {
   //   this._auth.signInWithFacebook()
   //     .then(() => this.onSignInSuccess());
@@ -35,22 +78,26 @@ export class AddSlabForm {
   //   console.log("Facebook display name ",this._auth.displayName());
   // }
 
-    addSlab(slab: any) {
-     let key = this.items.push(slab.value).key;
-      //console.log(slab.value);
-      this.updateCoords(key, this.coords);
-      this.updateStatus(key, 'Uncompleted');
-     //console.log(slab.value);
-    }
+  addSlab(slab: any) {
+    let key = this.items.push(slab.value).key;
+    //console.log(slab.value);
+    this.updateCoords(key, this.coords);
+    this.updateStatus(key, 'Uncompleted');
+    this.updatePic(key, this.picUrl);
+    //console.log(slab.value);
+  }
 
-    updateCoords(key: string, coords: object) {
-       this.items.update(key, { coords: coords });
-    }
-    updateStatus(key: string, status: string) {
-       this.items.update(key, { status: status });
-    }
+  updateCoords(key: string, coords: object) {
+    this.items.update(key, { coords: coords });
+  }
+  updateStatus(key: string, status: string) {
+    this.items.update(key, { status: status });
+  }
+  updatePic(key: string, url: string) {
+    this.items.update(key, { img_url: url });
+  }
 
-    callHT(){
-      
-    }
+  callHT() {
+
+  }
 }
